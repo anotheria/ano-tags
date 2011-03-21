4ef98cf2b1139d0f1401fe14e0193c80de249e09
Denis Metelin <denis.metelin@nazax.com>
2011-03-21 18:47:46 +0000

    repeat tag

---
+++ java/net/anotheria/tags/RepeatTag.java
@@ -0,0 +1,83 @@
+package net.anotheria.tags;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.BodyTagSupport;
+
+public class RepeatTag extends BodyTagSupport {
+
+	private static final long serialVersionUID = 1L;
+	protected String id = null;
+	protected String lastId = null;
+	protected int count;
+
+	protected int repeatsCount = 0;
+	protected boolean started = false;
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getLastId() {
+		return lastId;
+	}
+	public void setLastId(String last) {
+		this.lastId = last;
+	}
+
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	public int doStartTag() throws JspException {
+		if(count == 0)
+			return SKIP_BODY;
+		
+		repeatsCount = 1;
+		
+		if(id != null)
+			pageContext.setAttribute(id, Integer.valueOf(repeatsCount - 1));
+		if(lastId != null)
+			pageContext.setAttribute(lastId, repeatsCount == count);
+		
+		return EVAL_BODY_AGAIN;
+	}
+
+	public int doAfterBody() throws JspException {
+
+		// Render the output from this iteration to the output stream
+		if (bodyContent != null) {
+			TagUtils.writePrevious(pageContext, bodyContent.getString());
+			bodyContent.clearBody();
+		}
+		
+		// Decide whether to repeat
+		if (repeatsCount < count) {
+			repeatsCount++;
+			if(id != null)
+				pageContext.setAttribute(id, Integer.valueOf(repeatsCount - 1));
+			if(lastId != null)
+				pageContext.setAttribute(lastId, repeatsCount == count);
+			return EVAL_BODY_AGAIN;
+		}
+		
+		return SKIP_BODY;
+
+	}
+
+	public int doEndTag() throws JspException {
+		started = false;
+		return EVAL_PAGE;
+	}
+
+	public void release() {
+		super.release();
+		repeatsCount = 0;
+		started = false;
+	}
+
+}
