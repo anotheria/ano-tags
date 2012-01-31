ca3e536426017d8deb091bb72d48a689bb8f2510
Denis Metelin <denis.metelin@nazax.com>
2012-01-31 12:47:00 +0000

    Random Tag: writes or add to context random string of the given length

---
+++ java/net/anotheria/tags/RandomTag.java
@@ -0,0 +1,38 @@
+package net.anotheria.tags;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+
+import net.anotheria.util.IdCodeGenerator;
+import net.anotheria.util.StringUtils;
+
+public class RandomTag extends BaseTagSupport {
+
+	/**
+	 *	Serial Version ID 
+	 */
+	private static final long serialVersionUID = -808848857807480451L;
+	
+	private int length = IdCodeGenerator.CODE_LENGTH;
+
+	/**
+	 * Writes size of a specified collection to page contest.
+	 * 
+	 * @exception if
+	 *                collection not found or class is not supported
+	 */
+	@Override
+	public int doStartTag() throws JspException {
+		String rnd = IdCodeGenerator.generateCode(length);
+		if(StringUtils.isEmpty(getId()))
+			write(rnd);
+		else
+			pageContext.setAttribute(getId(), rnd, PageContext.PAGE_SCOPE);
+		return (SKIP_BODY);
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+}
